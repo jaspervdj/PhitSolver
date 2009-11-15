b9628aeeb850f802c43e1059b978f2b9bd4d69c6
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-11-15 17:05:21 +0100

    Some more readability, accept all characters.

--- PhitSolver.hs
+++ PhitSolver.hs
@@ -4,6 +4,7 @@ import Prelude as P
 import Data.Set as S
 import Control.Monad
 import Data.List (groupBy)
+import Data.Char
 
 data Tile = Tile { tileX :: Int
                  , tileY :: Int
@@ -11,6 +12,7 @@ data Tile = Tile { tileX :: Int
 
 type Piece = Set Tile
 type Board = Piece
+type Solution = [Piece]
 
 dimension :: (Tile -> Int) -> Piece -> Int
 dimension f piece = (findMax xs)
@@ -24,32 +26,46 @@ height = dimension tileY
 
 validPositions :: Board -> Piece -> [Piece]
 validPositions board piece = P.filter (canPutPiece board) allPositions
-    where allPositions = P.map (translate piece) allCoords
+    where allPositions :: [Piece]
+          allPositions = P.map (translate piece) allCoords
+
+          allCoords :: [(Int, Int)]
           allCoords = [(x, y) | x <- [0 .. (width board) - (width piece)],
                                 y <- [0 .. (height board) - (height piece)]]
+
+          translate :: Piece -> (Int, Int) -> Piece
           translate piece (x, y) = S.map (\(Tile tx ty) -> Tile (tx + x) (ty + y)) piece
+
+          canPutPiece :: Board -> Piece -> Bool
           canPutPiece board piece = all (`member` board) (toList piece)
 
 
-solve :: Board -> [Piece] -> [Piece] -> Maybe [Piece]
+solve :: Board -> [Piece] -> [Piece] -> Maybe Solution
 solve board piecesLeft added | P.null piecesLeft = Just added
                              | otherwise         = foldr mplus Nothing solutions
-    where solutions = P.map solve' positions
+    where solutions :: [Maybe Solution]
+          solutions = P.map solve' positions
+
+          solve' :: Piece -> Maybe Solution
           solve' piece = solve (putPiece board piece) (tail piecesLeft) (piece:added)
+
+          positions :: [Piece]
           positions = validPositions board (head piecesLeft)
+
+          putPiece :: Board -> Piece -> Board
           putPiece board piece = board \\ piece
 
 loadPiece :: [String] -> Piece
 loadPiece lines = fromList $ P.map toTile coords
     where coords = P.filter isTile $ concat $ P.map numberCols numberLines
           toTile (c, x, y) = Tile x y
-          isTile (c, x, y) = c == '#'
+          isTile (c, x, y) = not $ isSpace c
           numberLines = zip lines [1 ..]
           numberCols (line, lineNumber) = zipWith (\c n -> (c, n, lineNumber)) line [1 ..]
 
 pieces :: [String] -> [[String]]
 pieces = removeCrap . groupBy (\a b -> b /= "")
-    where removeCrap = P.filter (not . P.null) . P.map (P.filter (\a -> a /= ""))
+    where removeCrap = P.filter (not . P.null) . P.map (P.filter (/= ""))
 
 renderSolution :: Board -> [Piece] -> String
 renderSolution board solution = unlines $ P.map renderRow $ splitRows finalRender
